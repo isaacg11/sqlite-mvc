2070b59b255420a04da9ddfcab5df8dea6dfcdfc
Isaac Grey <isaacgrey@Isaacs-MacBook-Pro.local>
2018-06-22 08:26:21 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+// JS
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars').create({
+    defaultLayout: 'main'
+});
+const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('/Users/isaacgrey/Desktop/test/Chinook_Sqlite_AutoIncrementPKs.sqlite');
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get("port"), () => {
+    console.log(
+        'Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.'
+    );
+});
+
+let query1 = 'SELECT * FROM Artist INNER JOIN Album ON Artist.ArtistId = Album.ArtistId';
+let query1Results = [];
+
+db.each(query1, (err, row) => {
+    query1Results.push(row)
+});
+
+app.get('/', (request, response) => {
+    response.render('home')
+});
+
+app.get('/albums', (request, response) => {
+    response.render('albums', {albums: query1Results})
+});
+
